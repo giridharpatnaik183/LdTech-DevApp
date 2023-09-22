7b204a87c93572c6dadb4b3578ffa37398844457
Giridhar Sai Patnaik <94623129+giridharpatnaik183@users.noreply.github.com>
2023-09-22 16:03:55 +0530

    Update Contact.js

--- src/components/Subpages/Contact.js
+++ src/components/Subpages/Contact.js
@@ -21,7 +21,7 @@ const Contact = () => {
     e.preventDefault();
 
     try {
-      const response = await axios.post('http://localhost:5000/contact/submit', Data);
+      const response = await axios.post('http://13.234.23.179:3004/contact/submit', Data);
       console.log(response.data);
       setMessage('We will contact you soon');
       setError('');
